a3c5acc1bc47d92ff90f2e8df5764c06feebe572
Maxim Maltsev <mmi3220702@gmail.com>
2021-02-13 06:26:19 +0300

    task completion

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reversive(str) {
+
+    let positiveString = Math.abs(+str); // getting the module of the incoming string
+
+    let reversedString = String(positiveString).split(''); // splitting a string into an array of numbers
+
+    return reversedString.reverse().join(''); // reversing and joining array into a string
 }
